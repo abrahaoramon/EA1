c28bb108e60a96aff7859a061c213248d86cdd2f
Ramon Abrahao <abrahaoramon@ucl.br>
2022-04-04 17:25:33 -0300

    Somenta o encoder

--- code/atv1.c
+++ code/atv1.c
@@ -5,59 +5,82 @@
 
 #define INPUTS (char) char >= '0' && char <= '9' || char>= 'A' && char <= 'z'
 
+// Variável do tipo c_rle para compressão de texto
+struct c_rle 
+{
+    char caracter;
+    int count;
+};
+
 //Variáveis globais:
 char Fun[2]; // Tipo da função aplicada pelo usuário
 char Text[1000]; // Texto digitado pelo usuário
 char Str_Encode[1000]; // Saida da codificação
 char Str_Decode[1000]; // Saida da decodificação
+struct c_rle Coded[1000];
+int Psc_Coded, Serie;
 
 //Blocos de função:
-void Encoder(void);
-void Decoder(void);
-void C_Rle(void);
+void Encode(void);
+void Decode(void);
+void C_Rle(char in_text[], struct c_rle out_text[]);
 void D_Rle(void);
 
 //Procedimento principal:
 int main(void)
 {
   //Variávei locais:
-  int fun_encoder, fun_decoder;
+  int fun_encode, fun_decode;
      
 do
 {
     printf( "Escolha a funcao desejada 'C' para codificar ou 'D' para decodificar:\n");   
     gets(Fun);
-    fun_encoder = strcmp(Fun,"C"); // Comparação para codificar
-    fun_decoder = strcmp(Fun,"D"); // Comparação para decodificar
+    fun_encode = strcmp(Fun,"C"); // Comparação para codificar
+    fun_decode = strcmp(Fun,"D"); // Comparação para decodificar
 
-     if(fun_encoder==0 || fun_decoder==0)
+     if(fun_encode==0 || fun_decode==0)
     {
       printf( "Digite o texto:\n");
-      scanf("%s",&Text);
+      gets(Text);
     }
     else    
     {
        printf ("Funcao invalida\n\n");
      
     }   
-} while(fun_encoder!=0 && fun_decoder!=0); //Teste lógico para fim do laço de repetição
+} while(fun_encode!=0 && fun_decode!=0); //Teste lógico para fim do laço de repetição
         
-         if(fun_encoder==0)
+         if(fun_encode==0)
        {
-           Encoder();           
+           Encode();
+           C_Rle(Str_Encode,Coded);
+           printf("Codigo:\n");
+
+           for(int Psc_Coded=0;Psc_Coded< Serie;Psc_Coded++)
+           {
+               if(Coded[Psc_Coded].count > 1 )
+               {
+                   printf("%d%c",Coded[Psc_Coded].count,Coded[Psc_Coded].caracter);
+               }
+               else
+               {
+                   printf("%c",Coded[Psc_Coded].caracter);
+               }
+           }
+           printf("\n");
        }
-       if(fun_decoder==0)
+       if(fun_decode==0)
        {
-           Decoder();           
+           Decode();           
        }
        
     
-    system("pause");
     return 0;
 }
 
 //Função para codificação:
-void Encoder(void)
+void Encode(void)
 {
     //Variáveis locais:
     long psc_text= 0, psc_encoder = 0; // verificadores de posição
@@ -140,27 +163,47 @@ void Encoder(void)
         psc_text++;
         psc_encoder++;       
     }
-
-    printf ("Codificacao:\n");
-    puts(Str_Encode);
 } 
 
 //Função para decodificação:
-void Decoder(void)
+void Decode(void)
 {
     printf ( "Decodificador: \n");
     puts(Text);
 }
 
 // Função para compressão
-void C_Rle(void)
+void C_Rle(char in_text[], struct c_rle out_text[])
 {
-    printf ( "Decodificador: \n");
-    puts(Text);
+    int psc_comp, psc_in_text=0, ctrl; // variáveis de controle da função
+    int texto = strlen(in_text);
+    for(psc_comp=0; psc_comp<1000; psc_comp++)
+    {
+        for(ctrl=psc_in_text ; ctrl < texto ; ctrl++)
+        {
+            if(in_text[ctrl] != in_text[ctrl+1])
+            break;
+        }
+
+         out_text[psc_comp].count = ctrl-psc_in_text + 1;
+         out_text[psc_comp].caracter = in_text[psc_in_text];       
+
+        if( psc_in_text < texto )
+        {
+          psc_in_text = ctrl+1;
+        }
+        else
+        {
+            break;
+        }
+    }
+    Psc_Coded=psc_comp;
+    Serie=psc_comp;
 }
+
 // Função para descompressão
-void D_Rle(void)
+/*void D_Rle(void)
 {
     printf ( "Decodificador: \n");
     puts(Text);
-}
+}*\
